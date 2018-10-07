f8831748fae5ca94975342bf247b513ea4b02683
--global <miler.yoshio@gmail.com>
2018-10-07 12:44:29 +0900

    add Boss.import_csv command 181007 Ynakamura

--- app/models/boss.rb
+++ app/models/boss.rb
@@ -1,2 +1,27 @@
+require "csv"
+
 class Boss < ApplicationRecord
+
+  def self.import_csv
+    list = []
+    CSV.foreach("db/csv_data/boss_data.csv" , headers: true) do |boss|
+      list << {
+        name: boss["name"],
+        tribe: boss["tribe"]
+      }
+
+      # puts "#{boss}を読み込み"
+    end
+    Boss.create(list)
+  end
+  
+  def self.delete_all(password)
+    if password == "delete"
+      bosses = Boss.all
+      bosses.each do |boss|
+        boss.destroy
+      end
+    end
+  end
+
 end
